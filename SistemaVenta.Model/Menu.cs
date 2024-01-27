9288a2e89f4a57790b7925d4b5a61ba3b2830de2
Diego Nicolas Gonzalez Valenzuela <diegonzalezv93@gmail.com>
2024-01-27 19:43:18 -0300

    trayendo modelos desde DB con entity framework

---
+++ SistemaVenta.Model/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaVenta.Model;
+
+public partial class Menu
+{
+    public int IdMenu { get; set; }
+
+    public string? NombreMenu { get; set; }
+
+    public string? IconoMenu { get; set; }
+
+    public string? Url { get; set; }
+
+    public virtual ICollection<MenuRol> MenuRols { get; } = new List<MenuRol>();
+}
