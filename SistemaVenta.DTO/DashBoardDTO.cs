49a6af051dd4b6bf27f52930207ef2ff5b9d4df3
Diego Nicolas Gonzalez Valenzuela <diegonzalezv93@gmail.com>
2024-01-28 11:21:41 -0300

    venta y dashboard service

--- SistemaVenta.DTO/DashBoardDTO.cs
+++ SistemaVenta.DTO/DashBoardDTO.cs
@@ -10,6 +10,7 @@ namespace SistemaVenta.DTO
     {
         public int? TotalVentas { get; set; }
         public string? TotalIngresos { get; set; }
+        public int TotalProductos { get; set; }
         public List<VentaSemanaDTO> VentasUltimaSemana { get; set; }
     }
 }
