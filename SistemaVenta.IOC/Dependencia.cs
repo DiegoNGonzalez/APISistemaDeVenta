8e2ed84ca7a4ef0dc5328ce99adaeff8de4bfd97
Diego Nicolas Gonzalez Valenzuela <diegonzalezv93@gmail.com>
2024-01-28 11:38:08 -0300

    actualizando inyeccion de dependencias

--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -12,6 +12,8 @@ using SistemaVenta.DAL.Repositorios.Contrato;
 using SistemaVenta.DAL.Repositorios;
 
 using SistemaVenta.Utility;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.BLL.Servicios;
 
 namespace SistemaVenta.IOC
 {
@@ -28,6 +30,15 @@ namespace SistemaVenta.IOC
             services.AddScoped<IVentaRepository, VentaRepository>();
 
             services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            services.AddScoped<IRolService, RolService>();
+            services.AddScoped<IUsuarioService, UsuarioService>();
+            services.AddScoped<ICategoriaService, CategoriaService>();
+            services.AddScoped<IProductoService, ProductoService>();
+            services.AddScoped<IVentaService,VentaService>();
+            services.AddScoped<IDashBoardService, DashBoardService>();
+            services.AddScoped<IMenuService, MenuService>();
+
         }
     }
 }
