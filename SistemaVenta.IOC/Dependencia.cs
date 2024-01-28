8ee1ea2e250647761f325ec87bac8e66c40d62a8
Diego Nicolas Gonzalez Valenzuela <diegonzalezv93@gmail.com>
2024-01-28 00:32:30 -0300

    inyectando dependencia de mapeo

--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -11,6 +11,8 @@ using System.Threading.Tasks;
 using SistemaVenta.DAL.Repositorios.Contrato;
 using SistemaVenta.DAL.Repositorios;
 
+using SistemaVenta.Utility;
+
 namespace SistemaVenta.IOC
 {
     public static class Dependencia
@@ -24,6 +26,8 @@ namespace SistemaVenta.IOC
 
             services.AddTransient(typeof(IGenericRepository<>),typeof(GenericRepository<>));
             services.AddScoped<IVentaRepository, VentaRepository>();
+
+            services.AddAutoMapper(typeof(AutoMapperProfile));
         }
     }
 }
